d55d57ea195be016a2773717bef7ef003cb76491
lehichdevopler <lehdevopler@gmail.com>
2026-01-05 11:13:19 +0300

    Create hack.bat

---
+++ hack.bat
@@ -0,0 +1,8 @@
+echo -e "\033[32m\033[40m"
+clear
+echo "=== SYSTEM HACK INITIATED ==="
+echo "TARGET: SERVERS OF RKN"
+sleep 2
+echo "BYPASSING FIREWALL..."
+sleep 1
+cat /dev/urandom | hexdump -C | grep --color=auto "ca"
