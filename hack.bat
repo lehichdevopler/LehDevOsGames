4e87bd8edda2f8c5be43f562462802c93c9bf093
lehichdevopler <lehdevopler@gmail.com>
2026-01-06 12:19:55 +0300

    Delete hack.bat

--- hack.bat
+++
@@ -1,8 +0,0 @@
-echo -e "\033[32m\033[40m"
-clear
-echo "=== SYSTEM HACK INITIATED ==="
-echo "TARGET: SERVERS OF RKN"
-sleep 2
-echo "BYPASSING FIREWALL..."
-sleep 1
-cat /dev/urandom | hexdump -C | grep --color=auto "ca"
